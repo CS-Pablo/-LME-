6f958921a049a98e0e46ac484dff8c366c4daa2d
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-10-26 15:12:26 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,3 +3,45 @@ game ''
 description 'LME'
 lua54 'yes'
 version '1'
+
+shared_scripts {
+	'locale.lua',
+	'locales/*.lua',
+
+	'config.lua',
+	'config.weapons.lua',
+}
+
+server_scripts {
+
+
+}
+
+client_scripts {
+
+
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	'imports.lua',
+	'locale.js',
+	'html/ui.html',
+
+	'html/css/app.css',
+
+	'html/js/mustache.min.js',
+	'html/js/wrapper.js',
+	'html/js/app.js',
+
+
+}
+
+dependencies {
+	'/native:0x6AE51D4B',
+	'oxmysql',
+	'spawnmanager',
+}
