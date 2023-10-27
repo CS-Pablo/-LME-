8097d9c1eb185d654acc00ca08761790ea4de24e
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-10-27 16:44:08 +0200

    Update locale.js

--- locale.js
+++ locale.js
@@ -1,5 +1,34 @@
 const locale = require('./locale');
 
+const locale = {
+    fr: {
+      jobMenuTitle: "Menu des emplois",
+      jobMenuDescription: "Sélectionnez votre emploi :",
+      policeJob: "Policier",
+      emsJob: "Travailleur EMS",
+      bankerJob: "Banquier",
+      // Ajoutez d'autres chaînes de caractères
+    },
+    en: {
+      jobMenuTitle: "Job Menu",
+      jobMenuDescription: "Select your job:",
+      policeJob: "Police Officer",
+      emsJob: "EMS Worker",
+      bankerJob: "Banker",
+      // Ajoutez d'autres chaînes de caractères
+    },
+    es: {
+      jobMenuTitle: "Menú de trabajos",
+      jobMenuDescription: "Selecciona tu trabajo:",
+      policeJob: "Policía",
+      emsJob: "Trabajador de EMS",
+      bankerJob: "Bancario",
+      // Ajoutez d'autres chaînes de caractères
+    },
+    // Ajoutez d'autres langues au besoin
+  };
+  
+
 // Supposons que lang est la langue du joueur (par exemple, 'fr' pour le français)
 const lang = 'fr';
 
