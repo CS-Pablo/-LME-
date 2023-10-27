233c8f7b7e23f18f7a01f1316d09d69108602758
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-10-27 16:40:56 +0200

    Update locale.js

--- locale.js
+++ locale.js
@@ -1,14 +1,17 @@
-const Locales = {
-    'en': {
-      welcomeMessage: 'Welcome to the server!',
-      playerJoined: '%s has joined the server.',
-      // Ajoutez d'autres traductions ici
-    },
-    'fr': {
-      welcomeMessage: 'Bienvenue sur le serveur !',
-      playerJoined: '%s a rejoint le serveur.',
-      // Ajoutez d'autres traductions ici
-    },
-  };
-  
-  module.exports = Locales;
+const locale = require('./locale');
+
+// Supposons que lang est la langue du joueur (par exemple, 'fr' pour le français)
+const lang = 'fr';
+
+// Affiche le titre et la description du menu
+console.log(locale[lang].jobMenuTitle); // Affiche "Menu des emplois"
+console.log(locale[lang].jobMenuDescription); // Affiche "Sélectionnez votre emploi :"
+
+// Affiche les noms des emplois
+console.log(locale[lang].policeJob); // Affiche "Policier"
+console.log(locale[lang].emsJob); // Affiche "Travailleur EMS"
+console.log(locale[lang].bankerJob); // Affiche "Banquier"
+
+// Lorsqu'un joueur sélectionne un emploi (par exemple, EMS)
+const selectedJob = 'EMS';
+console.log(locale[lang].jobSelected.replace('%s', locale[lang][selectedJob])); // Affiche "Vous avez choisi l'emploi de Travailleur EMS."
