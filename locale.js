072a5ea0c190330aaf18fa3c22c657e499e3f0c0
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-10-27 16:37:56 +0200

    Update locale.js

---
+++ locale.js
@@ -0,0 +1,14 @@
+const Locales = {
+    'en': {
+      welcomeMessage: 'Welcome to the server!',
+      playerJoined: '%s has joined the server.',
+      // Ajoutez d'autres traductions ici
+    },
+    'fr': {
+      welcomeMessage: 'Bienvenue sur le serveur !',
+      playerJoined: '%s a rejoint le serveur.',
+      // Ajoutez d'autres traductions ici
+    },
+  };
+  
+  module.exports = Locales;
