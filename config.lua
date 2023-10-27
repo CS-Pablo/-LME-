95feadee099b6a76e82970bcfbc2025563517fca
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-10-27 05:33:45 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -11,6 +11,18 @@ Config.ResourceName = "lme_f6"
 -- Configurations du menu
 Config.MenuKey = 167  -- F6 (voir la liste des codes de touche)
 
+Config.MenuStyle = {
+	Margins = { left = 10, top = 10 }, -- [table] → Set the menu margins
+	BannerStyle = {
+		Color = { r = 150, g = 50, b = 50, a = 100 }, -- [table] → Set the banner color if no custom banner image is set
+		UseGlareEffect = true, -- [boolean] → Use the glare effect or not
+		UseInstructionalButtons = true, -- [boolean] → Use the instructionals buttons or not
+		ImageUrl = nil, -- [nil/string] → Set a custom image url if you want (if set, it will disable the Color configuration)
+		ImageSize = { Width = 512, Height = 128 }, -- [table] → Set the image (ImageUrl) size un pixels
+		widthOffset = 0, -- [integer] → Offset of the menu (default: 0, max: 100)
+	},
+}
+
 -- CONFIG ZONE FAUT FAIRE UN POLE EMPLOI WSH
 Config.Zones = {
     -- Zone 1
