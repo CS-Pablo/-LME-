a9327d195de78d3c6df79948a427f58ac545194a
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-10-27 02:15:49 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,50 +1,19 @@
----@diagnostic disable: undefined-global
-fx_version ''
-game ''
+fx_version 'cerulean'
+game 'gta5'
 description 'LME'
 lua54 'yes'
-version '1'
+version '1.0'
 
-shared_scripts {
-	'locale.lua',
-	'locales/*.lua',
+client_script 'menu.lua'
 
-	'config.lua',
-	'config.weapons.lua',
-}
-
-
-server_scripts {
-
-
-}
-
-client_scripts {
-	'menu.lua',
-
-
-}
-
-ui_page {
-	'html/ui.html'
-}
+ui_page 'html/ui.html'
 
 files {
-	'imports.lua',
-	'locale.js',
-	'html/ui.html',
-
-	'html/css/app.css',
-
-	'html/js/mustache.min.js',
-	'html/js/wrapper.js',
-	'html/js/app.js',
-
-
-}
-
-dependencies {
-	'/native:0x6AE51D4B',
-	'oxmysql',
-	'spawnmanager',
+    'imports.lua',
+    'locale.js',
+    'html/ui.html',
+    'html/css/app.css',
+    'html/js/mustache.min.js',
+    'html/js/wrapper.js',
+    'html/js/app.js'
 }
