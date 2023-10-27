fe3f3b83b5bbbcec3dc17ee8f28d7341d3f8fd7f
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-10-27 16:05:10 +0200

    Update html/js/wrapper.js

---
+++ html/js/wrapper.js
@@ -0,0 +1,5 @@
+// Attendez que le document soit prêt avant d'exécuter le code
+$(document).ready(function () {
+    // Votre code principal ici
+    // Par exemple, vous pouvez initialiser des éléments, ajouter des gestionnaires d'événements, etc.
+});
