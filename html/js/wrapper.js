679fa344c870aa3dacf576db1d3da8d1840859e0
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-10-27 17:48:04 +0200

    Update html/js/wrapper.js

--- html/js/wrapper.js
+++ html/js/wrapper.js
@@ -1,7 +1,9 @@
-const event = new Event('customEvent');
-document.dispatchEvent(event);
+
 // Attendez que le document soit prêt avant d'exécuter le code
 $(document).ready(function () {
     // Votre code principal ici
     // Par exemple, vous pouvez initialiser des éléments, ajouter des gestionnaires d'événements, etc.
+    const event = new Event('customEvent');
+document.dispatchEvent(event);
+
 });
