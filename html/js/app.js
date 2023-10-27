e33ffb846f40013934fa4dd9646b56e27888b00b
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-10-27 17:48:12 +0200

    Update html/js/app.js

--- html/js/app.js
+++ html/js/app.js
@@ -1,37 +1,39 @@
 document.addEventListener('customEvent', () => {
     // Effectuer des actions ici
+
+    $(document).ready(function () {
+        // Cache des éléments HTML
+        var menuContainer = $('#menu-container');
+        var menuOptions = $('#menu-options');
+    
+        // Cache de l'état du menu
+        var menuVisible = false;
+    
+        // Fonction pour afficher ou masquer le menu
+        function toggleMenu() {
+            menuVisible = !menuVisible;
+            if (menuVisible) {
+                menuContainer.show();
+            } else {
+                menuContainer.hide();
+            }
+        }
+    
+        // Écouteur pour la touche F6
+        $(document).on('keyup', function (event) {
+            if (event.keyCode === 117) { // Touche F6
+                toggleMenu();
+            }
+        });
+    
+        // Gestion des options de menu
+        menuOptions.on('click', '.menu-option', function () {
+            var selectedOption = $(this).text();
+            // Vous pouvez ajouter ici le code pour traiter l'option sélectionnée
+            console.log('Option sélectionnée : ' + selectedOption);
+            toggleMenu(); // Fermer le menu après sélection
+        });
+    });
+    
 });
 
-$(document).ready(function () {
-    // Cache des éléments HTML
-    var menuContainer = $('#menu-container');
-    var menuOptions = $('#menu-options');
-
-    // Cache de l'état du menu
-    var menuVisible = false;
-
-    // Fonction pour afficher ou masquer le menu
-    function toggleMenu() {
-        menuVisible = !menuVisible;
-        if (menuVisible) {
-            menuContainer.show();
-        } else {
-            menuContainer.hide();
-        }
-    }
-
-    // Écouteur pour la touche F6
-    $(document).on('keyup', function (event) {
-        if (event.keyCode === 117) { // Touche F6
-            toggleMenu();
-        }
-    });
-
-    // Gestion des options de menu
-    menuOptions.on('click', '.menu-option', function () {
-        var selectedOption = $(this).text();
-        // Vous pouvez ajouter ici le code pour traiter l'option sélectionnée
-        console.log('Option sélectionnée : ' + selectedOption);
-        toggleMenu(); // Fermer le menu après sélection
-    });
-});
