1e1054d0887e33016d5a07081a2d797d8d260883
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-11-02 00:04:39 +0100

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -1,35 +1,56 @@
+
+
 local menuVisible = false
+local jobSubMenu
 
-RMenu.Add('JobMenu', 'mainsubmenu', RageUI.CreateMenu("Job Menu", "Sélection rapide de votre boulot"))
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
+local mainMenu = RageUI.CreateMenu("Job Menu", "Sélection rapide de votre boulot");
 
-        if IsControlJustReleased(0, Config.MenuKey) then -- Touche F6
-            menuVisible = not menuVisible
-            if menuVisible then
-                RageUI.Visible(RMenu:Get('JobMenu', 'mainsubmenu'), true)
-            else
-                RageUI.Visible(RMenu:Get('JobMenu', 'mainsubmenu'), false)
-            end
-        end
 
-        RageUI.Button("JOB SELECT", "Sélection de votre emploi", {}, true, function(Hovered, Active, Selected)
-            if Selected then
-                -- Vous pouvez ajouter un sous-menu ici
-                local submenu = RMenu.Add('JobMenu', 'jobsubmenu', RageUI.CreateSubMenu("Sous-menu des emplois", "Sélectionnez votre emploi"))
-                
-                -- Ajoutez des boutons pour chaque emploi dans le sous-menu
-                for _, job in pairs(Config.Jobs) do
-                    RageUI.Button(job.label, "Sélectionnez le job " .. job.label, {}, true, function(Hovered, Active, Selected)
-                        if Selected then
-                            TriggerEvent('lme_f6:choisirJob', job.name)
-                        end
-                    end, submenu)
+    Citizen.CreateThread(function()
+        while true do
+            Citizen.Wait(0)
+    
+            if IsControlJustReleased(0, Config.MenuKey) then -- Touche F6
+                menuVisible = not menuVisible
+                if menuVisible then
+                    if not RageUI.Visible(mainMenu) then
+                        RageUI.Visible(mainMenu, true)
+                    end
+                else
+                    if RageUI.Visible(mainMenu) then
+                        RageUI.Visible(mainMenu, false)
+                    end
                 end
             end
-        end)
-    end
-end)
+    
+            if RageUI.Visible(mainMenu) then
+                RageUI.IsVisible(mainMenu, function()
+                    RageUI.Button("JOB SELECT", "Sélection de votre emploi", {}, true, function(Hovered, Active, Selected)
+                        if Selected then
+                            if not jobSubMenu then
+                                jobSubMenu = RageUI.CreateSubMenu(mainMenu, "Sous-menu des emplois", "Sélectionnez votre emploi")
+                                for _, job in pairs(Config.Jobs) do
+                                    RageUI.Button(job.label, "Sélectionnez le job " .. job.label, {}, true, function(Hovered, Active, Selected)
+                                        if Selected then
+                                            TriggerEvent('lme:choisirJob', job.name)
+                                        end
+                                    end, jobSubMenu)
+                                end
+                            end
+                        end
+                    end)
+                end)
+            end
+        end
+    end)
+
+
+
+
+
+
+
+
+
 
